4d7ccce4bb64191aaa6eb9729448cdb662725250
Keren <55347454+kerenk77@users.noreply.github.com>
2025-09-03 21:02:52 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
         <li><a href="schedule.html">Расписание</a></li>
         <li><a href="zoom.html">Классы ZOOM</a></li>
         <li><a href="https://www.beerot.ru/%d1%86%d0%b4%d0%b0%d0%ba%d0%b0-%d0%bf%d0%be%d0%b6%d0%b5%d1%80%d1%82%d0%b2%d0%be%d0%b2%d0%b0%d1%82%d1%8c-%d0%b1%d0%b5%d0%b5%d1%80%d0%be%d1%82-%d0%b8%d1%86%d1%85%d0%b0%d0%ba">Оплата</a></li>
+        <li><a href="lessons.html">Записи уроков</a></li>
         <li><a href="contact.html">Контакты</a></li>
         <li><a href="index.html#video-section">О школе</a></li>
       </ul>
